8037d2ba6838d1a7d45facd98b72e73e8fb24527
Wesley Azevedo <wesleyazevedogomes.contato@gmail.com>
2026-04-27 23:14:02 -0300

    feat: create product dto

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,1 +1,14 @@
-export class CreateProductDTO {}
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateProductDTO {
+  @IsNotEmpty({ message: 'O nome do produto é obrigatório' })
+  name!: string;
+
+  @IsNotEmpty({ message: 'O preço do produto é obrigatório' })
+  @IsNumber({}, { message: 'O preço deve ser um número' })
+  price!: number;
+
+  @IsOptional()
+  @IsNumber({}, { message: 'O estoque deve ser um número' })
+  stock?: number;
+}
